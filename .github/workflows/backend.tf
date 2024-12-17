5373ceee6b1af12318a95edae017b0e48acdaeec
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-17 22:48:08 +0530

    Update backend.tf

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = ""
-    key = ""
-    region = ""
+    bucket = "gokkul-12345"
+    key = "terraform/state/terraform.tfstate"
+    region = "us-east-1"
   }
 }
