c67bb72783301adb4c30189b2b81350e080b3381
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-22 18:38:38 +0530

    Create networking.tf

---
+++ networking.tf
@@ -0,0 +1,25 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "VPC"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "publicsubnet"
+  }
+}
+
+resouce "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "PrivateSubnet"
+   }
+}
