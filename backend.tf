05e8e55c9df467fc67aca2e3c05c62432bc4fb4f
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-22 18:22:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,26 @@
-terraform {
-  backend "s3" {
-    bucket = "gokkul-12345"
-    key = "terraform/state/terraform.tfstate"
-    region = "us-east-1"
+resource "aws_s3_bucket" "terraform_state"{
+
+  bucket   = "gokkul-12345"
+  acl      = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "terraformstateBucket"
+    Environment = "Infrastructure"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock"
+  name         = "terraform-lock-table"
+
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
