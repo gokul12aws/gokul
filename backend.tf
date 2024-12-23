9a44401169e81d98324ca06e97aadbf816a69f60
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-23 13:21:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -14,13 +14,13 @@ resource "aws_s3_bucket" "terraform_state"{
 }
 
 resource "aws_dynamodb_table" "terraform_lock"
-  name         = "terraform-lock-table"
+  name         = "dynamo-table"
 
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+  hash_key     = "Artist"
 
   attribute {
-    name = "LockID"
+    name = "Artist"
     type = "S"
   }
 }
