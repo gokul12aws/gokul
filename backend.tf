c79f20a236c7ca3ee28d1869a022df66d31384b0
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-23 13:27:24 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,26 +1,29 @@
-resource "aws_s3_bucket" "terraform_state"{
-
-  bucket   = "gokkul-12345"
-  acl      = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Name        = "terraformstateBucket"
-    Environment = "Infrastructure"
-  }
+resource "aws_s3_bucket" "terraform_state" {
+ bucket = "gokkul-12345"
+ acl    = "private"
+ versioning {
+   enabled = true
+ }
+ tags = {
+   Name        = "terraformstateBucket"
+   Environment = "Infrastructure"
+ }
 }
-
-resource "aws_dynamodb_table" "terraform_lock"
-  name         = "dynamo-table"
-
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "Artist"
-
-  attribute {
-    name = "Artist"
-    type = "S"
-  }
+resource "aws_dynamodb_table" "terraform_lock" {
+ name         = "dynamo-table"
+ billing_mode = "PAY_PER_REQUEST"
+ hash_key     = "Artist"
+ range_key    = "SongTitle"
+ attribute {
+   name = "Artist"
+   type = "S" # String
+ }
+ attribute {
+   name = "SongTitle"
+   type = "S" # String
+ }
+ tags = {
+   Name        = "terraform_lock_table"
+   Environment = "Infrastructure"
+ }
 }
