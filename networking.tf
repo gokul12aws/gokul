c508302aec4704e2b3a473efe116c9f897dd882a
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2024-12-23 13:33:46 +0530

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
-resouce "aws_subnet" "private_subnet" {
+resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.2.0/24"
 
