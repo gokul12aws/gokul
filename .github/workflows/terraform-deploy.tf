368faf935ab847f70ce21119652f0887a319450d
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2025-07-12 21:11:51 +0530

    Update terraform-deploy.tf

--- .github/workflows/terraform-deploy.tf
+++ .github/workflows/terraform-deploy.tf
@@ -9,13 +9,13 @@ jobs:
 
     steps:
     - name: checkout repository
-      uses: actions/checkout@v4
+      uses: actions/checkout@v2
 
     
     - name: set up terraform
       uses: hashicorp/setup-terraform@v1
       with:
-        terraform_version: 1.4.0
+        terraform_version: "1.4.0" 
         
     - name: aws credentials
       uses: aws-actions/configure-aws-credentials@v4
@@ -38,10 +38,3 @@ jobs:
     - name: Terraform Destroy
       run: terraform destroy -auto-approve
 
-
-
-
-     
-  
-      
-      
