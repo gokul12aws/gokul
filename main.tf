420f41c48fcce625a3200708ab767abc9226f1ef
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2025-07-12 19:11:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,95 @@
-terraform {
-  backend "s3" {
-    bucket          = "gokkul-12345"
-    key             = "networking/terraform.tfstate"
-    region          = "us-east-1" #N.virginia
-    dynamodb_table  = "dynamo-table"
-    encrypt         = true
-   }
-   required_providers {
-     aws = {
-       source  = "hashicorp/aws"
-       version = "~> 4.0"
-      }
-     }
-   }
-
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-module "compute" {
-  source      = "./compute"
-
+resource "aws_vpc" "gokul_vpc"{
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main_vpc"
+  }
 }
 
-module "networking" {
-  source     = "./networking"
-
+resource "aws_subnet" "subnet_a"{
+  Vpc_id            = aws_vpc.gokul_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_a"
+  }
 }
+
+resource "aws_subnet" "subnet_b" {
+  Vpc_id            = aws_vpc.gokul_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_b"
+  }
+} 
+
+resource "aws_internet_gateway" "igw"{
+  vpc_id = aws_vpc.gokul_vpc.id
+}
+
+resource "aws_route_table" "route_table"{
+  vpc_id = aws_vpc.gokul_vpc.id
+}
+
+resource "aws_route" "internet_route" {
+  route_table_id          = aws_route_table.route_table.id
+  destination_cidr_block  = "0.0.0.0/0"
+  gateway_id              = aws_internet_gateway_igw.id
+}
+
+resource "aws_security_group" "allow_http"{
+  vpc.id = aws_vpc.gokul_vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+}
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+}
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_security_group" "allow_https"{
+  vpc.id = aws_vpc.gokul_vpc.id
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+}
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+  }
+}
+
+
+
+
+
+
 
 
   
