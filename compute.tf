6134e25d565a9392df866a85e480ca7c8528da0f
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2026-01-10 12:22:00 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,11 +1,17 @@
 resource "aws_instance" "instance_gokul" {
-  count                  = 3
-  ami                    = "ami-020cba7c55df1f615"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  count         = 3
+  ami           = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.subnet_pub.id
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_http.id,
+    aws_security_group.allow_https.id
+  ]
 
   tags = {
     Name = "Helloworld-${count.index + 1}"
   }
 }
+
