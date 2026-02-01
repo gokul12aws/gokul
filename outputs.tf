bd68e8070e9d7a10d553a9c536f7e61bb4d9ac82
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2026-02-01 16:08:10 +0530

    Add outputs for EKS cluster and VPC

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Cluster security group"
+  value       = module.eks.cluster_security_group_id
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
