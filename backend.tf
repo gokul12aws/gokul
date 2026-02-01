242015259845bb621ef5d435629b807db41a1793
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2026-02-01 18:26:52 +0530

    Update DynamoDB table name in backend configuration

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "my-terraform-state-bucket112"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    dynamodb_table = "terraform-lock11"
     encrypt        = true
   }
 }
