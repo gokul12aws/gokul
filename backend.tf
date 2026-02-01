8423b41dea50a40df37e50600f65211027e80caa
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2026-02-01 18:23:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = "my-terraform-state-bucket112"
     key            = "eks/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock"
