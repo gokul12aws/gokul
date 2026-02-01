a841729473c1b16dd38bf95a85d06f1aed950660
gokul12aws <109579960+gokul12aws@users.noreply.github.com>
2026-02-01 16:13:29 +0530

    Change availability zones for VPC configuration

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
 
-  azs             = ["us-east-1a", "us-east-1b"]
+  azs             = ["ap-south-1", "ap-south-2"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
